8daf2779b82f0402bf301da6ae4bc8ef6a6dbefb
Italo Izaac <italoiz.dev@gmail.com>
2021-01-07 12:06:36 -0300

    Add pods to gitignore

--- ios/Pods/Target Support Files/Pods-appwithcustomnavigator-appwithcustomnavigatorTests/Pods-appwithcustomnavigator-appwithcustomnavigatorTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_appwithcustomnavigator_appwithcustomnavigatorTests : NSObject
-@end
-@implementation PodsDummy_Pods_appwithcustomnavigator_appwithcustomnavigatorTests
-@end
